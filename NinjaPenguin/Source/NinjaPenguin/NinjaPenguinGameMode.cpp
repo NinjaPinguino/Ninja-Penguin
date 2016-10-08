4637bbbd461c0777b1b7852205d300a91cef2092
Miguel Torres <torresCodes@icloud.com>
2016-10-08 14:29:32 -0700

    Ninja Penguin
    First Building Blockout

---
+++ NinjaPenguin/Source/NinjaPenguin/NinjaPenguinGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NinjaPenguin.h"
+#include "NinjaPenguinGameMode.h"
+
+
+
+
---
+++ NinjaPenguin/Source/NinjaPenguin/NinjaPenguinGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "NinjaPenguinGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NINJAPENGUIN_API ANinjaPenguinGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
